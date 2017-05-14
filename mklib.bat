5858d08066c633e1ca2df7fb4929427b9bf594e4
MITSUNARI Shigeo <herumi@nifty.com>
2017-05-14 18:34:41 +0900

    remove rem to compile bls_if.cpp

--- mklib.bat
+++ mklib.bat
@@ -4,7 +4,7 @@ echo make bls.lib
 cl /c %CFLAGS% src\bls.cpp
 lib /OUT:lib\bls.lib /nodefaultlib bls.obj %LDFLAGS%
 rem echo make lib_if.lib
-rem cl /c %CFLAGS% src\bls_if.cpp
+cl /c %CFLAGS% src\bls_if.cpp
 rem lib /OUT:lib\bls_if.lib /nodefaultlib bls_if.obj %LDFLAGS%
 echo make bls256.dll
 link /nologo /DLL /OUT:bin\bls256.dll bls.obj bls_if.obj %LDFLAGS% /implib:lib\bls_if256.lib
