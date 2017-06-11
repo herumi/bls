55c8bb26a38b21418335e249d7d1a301e0029309
MITSUNARI Shigeo <herumi@nifty.com>
2017-06-12 05:01:29 +0900

    fix include path

--- mkdll.bat
+++ mkdll.bat
@@ -1,4 +1,4 @@
 @echo off
 call setvar.bat
 echo make bls256.dll
-cl /LD src\bls_c.cpp ..\mcl\src\fp.cpp -Iinclude -I../mcl -I../mcl/include -I../cybozulib_ext/include /MT /W4 /DMCLBN_FP_UNIT_SIZE=4 /Ox /EHsc /DNOMINMAX /DNDEBUG /DMCL_NO_AUTOLINK /link /out:bin\bls256.dll /implib:lib\bls256.lib /LIBPATH:..\cybozulib_ext\lib
+cl /LD src\bls_c.cpp ..\mcl\src\fp.cpp -Iinclude -I../mcl -I../xbyak -I../mcl/include -I../cybozulib/include -I../cybozulib_ext/include /MT /W4 /DMCLBN_FP_UNIT_SIZE=4 /Ox /EHsc /DNOMINMAX /DNDEBUG /DMCL_NO_AUTOLINK /link /out:bin\bls256.dll /implib:lib\bls256.lib /LIBPATH:..\cybozulib_ext\lib
