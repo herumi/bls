9feb40dbffe802f69c21656e7bbc8bf3063b4c4d
MITSUNARI Shigeo <herumi@nifty.com>
2018-08-01 09:25:54 +0900

    forget to commit

--- ffi/go/bls/bls.go
+++ ffi/go/bls/bls.go
@@ -1,6 +1,8 @@
 package bls
 
 /*
+#cgo CFLAGS:-I../../../include -I../../../../mcl/include/
+#cgo LDFLAGS:-L../../../lib
 #cgo CFLAGS:-DMCLBN_FP_UNIT_SIZE=6
 #cgo LDFLAGS:-lbls384_dy -lcrypto -lgmp -lgmpxx -lstdc++
 #include <bls/bls.h>
