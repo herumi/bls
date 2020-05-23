a750a3b1fd5718e6e494c27a8767a0cba42b3780
MITSUNARI Shigeo <herumi@nifty.com>
2020-05-24 08:38:12 +0900

    cmake supports -DBLS_ETH=1

--- sample/minsample.c
+++ sample/minsample.c
@@ -1,17 +1,25 @@
-#ifdef BLS_ETH
 #include <bls/bls384_256.h>
-#else
-#include <bls/bls384.h>
-#endif
 #include <stdio.h>
 
 int main()
 {
+#ifdef BLS_ETH
+	puts("BLS_ETH mode");
+#else
+	puts("no BLS_ETH mode");
+#endif
 	int r = blsInit(MCL_BLS12_381, MCLBN_COMPILED_TIME_VAR);
 	if (r != 0) {
-		printf("err r=%d\n", r);
+		printf("err blsInit %d\n", r);
 		return 1;
 	}
+#ifdef BLS_ETH
+	r = blsSetETHmode(BLS_ETH_MODE_DRAFT_07);
+	if (r != 0) {
+		printf("err blsSetETHmode %d\n", r);
+		return 1;
+	}
+#endif
 	blsSecretKey sec;
 	blsSecretKeySetDecStr(&sec, "13", 2);
 	blsPublicKey pub;
@@ -20,7 +28,7 @@ int main()
 	const char *msg = "abc";
 	size_t msgSize = 3;
 	blsSign(&sig, &sec, msg, msgSize);
-	printf("%d\n", blsVerify(&sig, &pub, msg, msgSize));
-	printf("%d\n", blsVerify(&sig, &pub, "xyz", msgSize));
+	printf("verify correct message %d\n", blsVerify(&sig, &pub, msg, msgSize));
+	printf("verify wrong message %d\n", blsVerify(&sig, &pub, "xyz", msgSize));
 	return 0;
 }
