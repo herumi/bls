eeec2a16ed2acc121820e82f8fefc0477906ef5c
MITSUNARI Shigeo <herumi@nifty.com>
2020-05-28 09:09:55 +0900

    add sample of k_of_nSample

--- sample/minsample.c
+++ sample/minsample.c
@@ -1,6 +1,52 @@
 #include <bls/bls384_256.h>
 #include <stdio.h>
 
+void simpleSample()
+{
+	blsSecretKey sec;
+	blsSecretKeySetDecStr(&sec, "13", 2);
+	blsPublicKey pub;
+	blsGetPublicKey(&pub, &sec);
+	blsSignature sig;
+	const char *msg = "abc";
+	size_t msgSize = 3;
+	blsSign(&sig, &sec, msg, msgSize);
+	printf("verify correct message %d\n", blsVerify(&sig, &pub, msg, msgSize));
+	printf("verify wrong message %d\n", blsVerify(&sig, &pub, "xyz", msgSize));
+}
+
+void k_of_nSample()
+{
+#define N 5
+#define K 3
+	blsSecretKey secs[K]; // msk = secs[0]
+	for (int i = 0; i < K; i++) {
+		blsSecretKeySetByCSPRNG(&secs[i]);
+	}
+	// get master public key
+	blsPublicKey mpk;
+	blsGetPublicKey(&mpk, &secs[0]);
+	blsId ids[N];
+	blsSignature sigs[N];
+	const char *msg = "abc";
+	const size_t msgSize = strlen(msg);
+	for (int i = 0; i < N; i++) {
+		blsIdSetInt(&ids[i], i + 1);
+		blsSecretKey sec;
+		// sec is a secret key for ids[i] generated from secs[0..K-1]
+		blsSecretKeyShare(&sec, secs, K, &ids[i]);
+		blsSign(&sigs[i], &sec, msg, msgSize);
+	}
+	{
+		blsSignature sig;
+		// sig is recoverd from sigs[i] and ids [i] for i = 1, 2, 3(=K)
+		blsSignatureRecover(&sig, &sigs[1], &ids[1], K);
+		printf("verify=%d\n", blsVerify(&sig, &mpk, msg, msgSize));
+	}
+#undef K
+#undef N
+}
+
 int main()
 {
 #ifdef BLS_ETH
@@ -20,15 +66,7 @@ int main()
 		return 1;
 	}
 #endif
-	blsSecretKey sec;
-	blsSecretKeySetDecStr(&sec, "13", 2);
-	blsPublicKey pub;
-	blsGetPublicKey(&pub, &sec);
-	blsSignature sig;
-	const char *msg = "abc";
-	size_t msgSize = 3;
-	blsSign(&sig, &sec, msg, msgSize);
-	printf("verify correct message %d\n", blsVerify(&sig, &pub, msg, msgSize));
-	printf("verify wrong message %d\n", blsVerify(&sig, &pub, "xyz", msgSize));
+	simpleSample();
+	k_of_nSample();
 	return 0;
 }
