c279d023fb9b6495cca3f9ea2c5232719d76764c
MITSUNARI Shigeo <herumi@nifty.com>
2020-05-31 10:45:40 +0900

    add comments to minsample.c

--- sample/minsample.c
+++ sample/minsample.c
@@ -1,6 +1,7 @@
 #include <bls/bls384_256.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 void simpleSample()
 {
@@ -18,31 +19,99 @@ void simpleSample()
 
 void k_of_nSample()
 {
-#define N 5
-#define K 3
-	blsSecretKey secs[K]; // msk = secs[0]
-	for (int i = 0; i < K; i++) {
-		blsSecretKeySetByCSPRNG(&secs[i]);
-	}
-	// get master public key
+#define N 5 // you can increase
+#define K 3 // fixed
 	blsPublicKey mpk;
-	blsGetPublicKey(&mpk, &secs[0]);
 	blsId ids[N];
+	blsSecretKey secs[N];
+	blsPublicKey pubs[N];
 	blsSignature sigs[N];
+
 	const char *msg = "abc";
 	const size_t msgSize = strlen(msg);
+
+	// All ids must be non-zero and different from each other.
 	for (int i = 0; i < N; i++) {
 		blsIdSetInt(&ids[i], i + 1);
-		blsSecretKey sec;
-		// sec is a secret key for ids[i] generated from secs[0..K-1]
-		blsSecretKeyShare(&sec, secs, K, &ids[i]);
-		blsSign(&sigs[i], &sec, msg, msgSize);
 	}
+
+	/*
+		A trusted third party distributes N secret keys.
+		If you want to avoid it, then see DKG (distributed key generation),
+		which is out of the scope of this library.
+	*/
 	{
-		blsSignature sig;
-		// sig is recoverd from sigs[i] and ids [i] for i = 1, 2, 3(=K)
-		blsSignatureRecover(&sig, &sigs[1], &ids[1], K);
-		printf("verify=%d\n", blsVerify(&sig, &mpk, msg, msgSize));
+		blsSecretKey msk[K];
+		for (int i = 0; i < K; i++) {
+			blsSecretKeySetByCSPRNG(&msk[i]);
+		}
+		// share secret key
+		for (int i = 0; i < N; i++) {
+			blsSecretKeyShare(&secs[i], msk, K, &ids[i]);
+		}
+
+		// get master public key
+		blsGetPublicKey(&mpk, &msk[0]);
+
+		// each user gets their own public key
+		for (int i = 0; i < N; i++) {
+			blsGetPublicKey(&pubs[i], &secs[i]);
+		}
+	}
+
+	// each user signs the message
+	for (int i = 0; i < N; i++) {
+		blsSign(&sigs[i], &secs[i], msg, msgSize);
+	}
+
+	// The master signature can be recovered from any K subset of N sigs.
+	{
+		assert(K == 3);
+		blsSignature subSigs[K];
+		blsId subIds[K];
+		for (int i = 0; i < N; i++) {
+			subSigs[0] = sigs[i];
+			subIds[0] = ids[i];
+			for (int j = i + 1; j < N; j++) {
+				subSigs[1] = sigs[j];
+				subIds[1] = ids[j];
+				for (int k = j + 1; k < N; k++) {
+					subSigs[2] = sigs[k];
+					subIds[2] = ids[k];
+					// recover sig from subSigs[K] and subIds[K]
+					blsSignature sig;
+					blsSignatureRecover(&sig, subSigs, subIds, K);
+					if (!blsVerify(&sig, &mpk, msg, msgSize)) {
+						printf("ERR can't recover i=%d, j=%d, k=%d\n", i, j, k);
+						return;
+					}
+				}
+			}
+		}
+		puts("recover test1 is ok");
+	}
+
+	// any K-1 of N sigs can't recover
+	{
+		assert(K == 3);
+		blsSignature subSigs[K - 1];
+		blsId subIds[K - 1];
+		for (int i = 0; i < N; i++) {
+			subSigs[0] = sigs[i];
+			subIds[0] = ids[i];
+			for (int j = i + 1; j < N; j++) {
+				subSigs[1] = sigs[j];
+				subIds[1] = ids[j];
+				// can't recover sig from subSigs[K-1] and subIds[K-1]
+				blsSignature sig;
+				blsSignatureRecover(&sig, subSigs, subIds, K - 1);
+				if (blsVerify(&sig, &mpk, msg, msgSize)) {
+					printf("ERR verify must always fail. i=%d, j=%d\n", i, j);
+					return;
+				}
+			}
+		}
+		puts("recover test2 is ok");
 	}
 #undef K
 #undef N
