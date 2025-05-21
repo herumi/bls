c549d50e245a09c9b6dabed2d7c2787f354d59da
MITSUNARI Shigeo <herumi@nifty.com>
2025-05-21 18:35:16 +0900

    update cgo setting for v3

--- ffi/go/bls/link.go
+++ ffi/go/bls/link.go
@@ -2,6 +2,6 @@ package bls
 
 /*
 #cgo CFLAGS:-I ../../../include -I ../../../mcl/include
-#cgo LDFLAGS:-lmcl -L../../../lib -L../../../mcl/lib
+#cgo LDFLAGS:-lbls384_256 -L../../../lib -lmcl -L../../../../mcl/lib -lstdc++
 */
 import "C"
