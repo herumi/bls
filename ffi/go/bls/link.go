18bf4c08f98304a6c859f7839177b30ddfb077e5
MITSUNARI Shigeo <herumi@nifty.com>
2025-05-22 15:25:23 +0900

    fix path to lib

--- ffi/go/bls/link.go
+++ ffi/go/bls/link.go
@@ -2,6 +2,6 @@ package bls
 
 /*
 #cgo CFLAGS:-I ../../../include -I ../../../mcl/include
-#cgo LDFLAGS:-lbls384_256 -L../../../lib -lmcl -L../../../../mcl/lib -lstdc++
+#cgo LDFLAGS:-lbls384_256 -L../../../lib -lmcl -L../../../mcl/lib -lstdc++
 */
 import "C"
